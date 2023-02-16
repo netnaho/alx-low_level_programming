e5083d6e24bd667ca3ffd8503a63b6ad95c2fe1a
nahom <nahomesayas3@gmail.com>
2023-02-16 14:21:08 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 # include <stdio.h>
 /**
- * main - program print some message
- *
+ * main - A program that prints a line using the printf function
  * Return: 0 (Success)
  */
 int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
