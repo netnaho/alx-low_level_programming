4bb84a3b71e83dc5f9508805881d56c42601d6de
netnaho <87896136+netnaho@users.noreply.github.com>
2023-04-07 03:11:22 -0800

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * get_bit - Returns the value of the bit at a given index.
+ * get_bit - returns the value of the bit at a given index.
  *
- * @n: The unsigned long int to check.
- * @index: The index of the bit to get (0 is the least significant bit).
+ * @n: the unsigned long int to check.
+ * @index: the index of the bit to get (0 is the least significant bit).
  *
- * Return: The value of the bit at the given index (0 or 1), or -1 if an error occurred.
+ * Return: the value of the bit at the given index (0 or 1), or -1 if an error occurred.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8)
-	{
-		/* Error: index out of range */
-		return -1;
-	}
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
 	unsigned long int mask = 1UL << index;
 	unsigned long int masked_n = n & mask;
 
-	return masked_n > 0 ? 1 : 0;
+	return (masked_n != 0) ? 1 : 0;
 }
