99220354190407193e12aeb9fc5b0ad7649552b7
netnaho <87896136+netnaho@users.noreply.github.com>
2023-04-07 03:44:52 -0800

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * clear_bit - clear bit to 0 at a given index
- * @n: number
- * @index: index within binary number
- * Return: -1 if false, or -1 if true
+ * flip_bits - Counts the number of bits needed to be
+ * flipped to get from one number to another.
+ * @n: The number.
+ * @m: To flip n to.
+ * Return: The necessary number of bits to flip to get from n to m.
  */
-
-int clear_bit(unsigned long int *n, unsigned int index)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int number = 1;
+	unsigned long int used_diff, used_bits = 0;
 
-	if (index > (sizeof(unsigned long int) * 8))
+	used_diff = n ^ m;
 
-		return (-1);
+	while (diff > 0)
+	{
+		used_bits += (used_diff & 1);
+		used_diff >>= 1;
+	}
 
-	number = ~(number << index);
-
-	*n = *n & number;
-
-	return (1);
+	return (used_bits);
 }
