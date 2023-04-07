c4d77b7cca43d825d7081b9f70826659a1662a5c
netnaho <87896136+netnaho@users.noreply.github.com>
2023-04-07 02:05:16 -0800

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,16 +2,23 @@
 #include <stdio.h>
 
 /**
- * print_binary - converts unsigned int to binary
- * @n: unsigned int
- * Return: binary
- */
+
+print_binary - converts unsigned int to binary
+
+@n: unsigned int
+
+Return: binary
+*/
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		/*right shift operator*/
-		/*shifts all bits towards right by certain numbf 1 bits*/
-		print_binary(n >> 1);
+unsigned long int mask = 1;
+int bitCount = sizeof(n) * 8;
 
-	_putchar((n & 1) + '0');
+while (bitCount--)
+{
+if (n & (mask << bitCount))
+_putchar('1');
+else
+_putchar('0');
+}
 }
