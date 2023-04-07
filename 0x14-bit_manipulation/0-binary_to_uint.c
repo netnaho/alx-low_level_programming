8446b9addf4f11512be34147a83eba9874fe3f4f
netnaho <87896136+netnaho@users.noreply.github.com>
2023-04-06 20:41:14 -0800

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,26 +9,26 @@
 unsigned int binary_to_uint(const char *b)
 {
 
-	int night = 0, j;
+	int meshet = 0, j;
 	unsigned int number = 0, non_binary = 0;
 
 	if (b == NULL)
 		return (non_binary);
 
-	while (b[night] != '\0')
-		night++;
-	night -= 1;
+	while (b[meshet] != '\0')
+		meshet++;
+	meshet -= 1;
 	j = 0;
 	while (b[j])
 	{
-		if ((b[j] != '0') && (b[j] != '1')) /*&& logical AND operator*/
+		if ((b[j] != '0') && (b[j] != '1'))
 			return (non_binary);
 
 		if (b[j] == '1')
-			number += (1 * (1 << night)); /*<< bitwise left shift operator*/
+			number += (1 * (1 << meshet));
 		j++;
 
-		night--;
+		meshet--;
 	}
 	return (number);
 }
