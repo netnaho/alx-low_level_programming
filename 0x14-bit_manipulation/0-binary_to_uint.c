c3f14ee41f6520454825707fa2e2393c6c5e4b8e
netnaho <87896136+netnaho@users.noreply.github.com>
2023-04-06 20:25:14 -0800

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,24 +4,32 @@
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to a string of 0 and 1 chars
  *
- * Return: the converted number, or 0 if there is an invalid char or b is NULL
+ * Return: the converted number, or 0 if there is one or more chars in the
+ *         string b that is not 0 or 1, or b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
+	unsigned int result = 0, base = 1;
 
 	if (b == NULL)
 		return 0;
 
-	for (int i = 0; b[i] != '\0'; i++)
+	int len = 0;
+	while (b[len] != '\0')
+		len++;
+
+	for (int i = len - 1; i >= 0; i--)
 	{
-		if (b[i] == '0')
-			res <<= 1;
-		else if (b[i] == '1')
-			res = (res << 1) | 1;
+		if (b[i] == '0' || b[i] == '1')
+		{
+			result += (b[i] - '0') * base;
+			base *= 2;
+		}
 		else
-			return 0;
+		{
+			return 0; /* invalid character */
+		}
 	}
 
-	return res;
+	return result;
 }
