21caaacaa08882d5e889d96a03678d508382ad3f
netnaho <87896136+netnaho@users.noreply.github.com>
2023-04-06 20:37:31 -0800

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,35 +1,34 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointer to a string of 0 and 1 chars
- *
- * Return: the converted number, or 0 if there is one or more chars in the
- *         string b that is not 0 or 1, or b is NULL
+ * binary_to_uint- convert binary to unsigned int
+ * @b: binary
+ * Return: unsigned int
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0, base = 1;
+
+	int night = 0, j;
+	unsigned int number = 0, non_binary = 0;
 
 	if (b == NULL)
-		return 0;
+		return (non_binary);
 
-	int len = 0;
-	while (b[len] != '\0')
-		len++;
-
-	for (int i = len - 1; i >= 0; i--)
+	while (b[night] != '\0')
+		night++;
+	night -= 1;
+	j = 0;
+	while (b[j])
 	{
-		if (b[i] == '0' || b[i] == '1')
-		{
-			result += (b[i] - '0') * base;
-			base *= 2;
-		}
-		else
-		{
-			return 0; /* invalid character */
-		}
-	}
+		if ((b[j] != '0') && (b[j] != '1')) /*&& logical AND operator*/
+			return (non_binary);
 
-	return result;
+		if (b[j] == '1')
+			number += (1 * (1 << night)); /*<< bitwise left shift operator*/
+		j++;
+
+		night--;
+	}
+	return (number);
 }
