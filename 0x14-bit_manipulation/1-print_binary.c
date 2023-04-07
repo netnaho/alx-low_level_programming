49b3f620176fa41457f3d2b2547a1e2931385809
netnaho <87896136+netnaho@users.noreply.github.com>
2023-04-07 02:22:18 -0800

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,17 +10,21 @@ void print_binary(unsigned long int n)
 	unsigned long int mask = 1UL << (sizeof(n) * 8 - 1);
 	int started = 0;
 
-	while (mask > 0) {
-		if (n & mask) {
+	while (mask > 0)
+	{
+		if (n & mask)
+		{
 			started = 1;
 			_putchar('1');
-		} else if (started) {
-			_putchar('0');
 		}
+		else if (started) 
+			_putchar('0');
+		
 		mask >>= 1;
 	}
 
-	if (!started) {
+	if (!started)
+	
 		_putchar('0');
-	}
+	
 }
