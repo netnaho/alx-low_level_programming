998c510292ed4985d1dd9a8defb327c34aaabf09
netnaho <87896136+netnaho@users.noreply.github.com>
2023-04-10 10:56:54 -0800

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,49 +4,49 @@
 #include "main.h"
 
 /**
- * append_text_to_file - apends text to file
- * @filename: path to file
+ * append_text_to_file - the text file to be appended
+ * @filename: file to path
  * @text_content: content
  * Return: 1 or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t w;
+	int dl;
+	ssize_t k;
 	int size;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	dl = open(filename, O_WRONLY | O_APPEND);
 
-	if (fd == -1)
+	if (dl == -1)
 		return (-1);
 
 	if (!text_content)
 	{
-		close(fd);
+		close(dl);
 		return (1);
 	}
 
 	size = _strlen(text_content);
-	w = write(fd, text_content, size);
+	k = write(dl, text_content, size);
 
-	if (w == -1)
+	if (k == -1)
 	{
-		close(fd);
+		close(dl);
 		return (-1);
 	}
-	close(fd);
+	close(dl);
 	return (1);
 }
 
 /**
- * _strlen - len
+ * _strlen - length
  *
- * @s: is a pointer to a char
+ * @s: is a charachter pointer
  *
- * Return: Always 0.
+ * Return: return 0.
  */
 
 int _strlen(const char *s)
