ebed97f2b7fa3a55e8e6ff496cc599f6cc963582
netnaho <87896136+netnaho@users.noreply.github.com>
2023-04-10 10:53:06 -0800

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,16 +4,16 @@
 #include "main.h"
 
 /**
- * create_file - append to file
- * @filename: path to file
- * @text_content: content
- * Return: chars read
+ * create_file - the file to be appended on
+ * @filename: the file path
+ * @text_content: the content
+ * Return: the read charachters
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t w;
+	int dl;
+	ssize_t k;
 	int size;
 	char *mem;
 
@@ -21,39 +21,39 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
+	dl = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (dl == -1)
 		return (-1);
 	if (!text_content)
 	{
-		close(fd);
+		close(dl);
 		return (1);
 	}
 	size = _strlen(text_content);
 	mem = malloc(sizeof(char) * size);
 	if (!mem)
 	{
-		close(fd);
+		close(dl);
 		return (-1);
 	}
-	w = write(fd, text_content, size);
-	if (w == -1)
+	k = write(dl, text_content, size);
+	if (k == -1)
 	{
-		close(fd);
+		close(dl);
 		free(mem);
 		return (-1);
 	}
-	close(fd);
+	close(dl);
 	free(mem);
 	return (1);
 }
 
 /**
- * _strlen - len
+ * _strlen - length
  *
- * @s: is a pointer to a char
+ * @s: is charachter pointer
  *
- * Return: Always 0.
+ * Return: return 0.
  */
 
 int _strlen(const char *s)
